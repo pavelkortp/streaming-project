5ae6590058995ba294312349e35bd30400274fa8
Pavlo Zhurbytskyi <92216297+pavelkortp@users.noreply.github.com>
2025-06-18 15:06:33 +0300

    [ISSUE-3][Nest] DatabaseModule: update db connection (#6)
    * update db connection to use only DATABASE_URL
    
    * lint

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,24 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+config();
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        const obj: TypeOrmModuleOptions = {
-          type: 'postgres',
-          host: config.get<string>('DB_HOST'),
-          port: config.get<number>('DB_PORT'),
-          username: config.get<string>('DB_USER'),
-          password: config.get<string>('DB_PASSWORD'),
-          database: config.get<string>('DB_NAME'),
-        };
-        console.log(obj);
-        return obj;
-      },
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        ssl: true,
+        url: config.get<string>('DATABASE_URL'),
+        entities: ['../**/*.entity.js'],
+      }),
     }),
   ],
 })
